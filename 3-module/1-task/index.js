a4c1215d7286a45f8dc9e524e8ecb642f405f0f3
Igor <igorchuvatkin@gmail.com>
2020-12-12 15:09:53 +0300

    Filte array and transform to camelCase

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -9,9 +9,11 @@ let masha = { name: 'Маша', age: 28 };
 
 let users = [ vasya, petya, masha ];
   function namify(users) {
-    return name = users.map(function (elem){
+    let result = []
+    return result = users.map(function (elem){
       return elem.name
     }
     )
   };
   let names = namify(users);
+  console.log(names)
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,6 +2,20 @@
  * @param {string} str
  * @returns {string}
  */
-function camelize(str) {
-  // ваш код...
+function camelize(str){
+  // debugger
+  let arr = str.split( '-' );
+  // console.log( arr );
+  let result = arr.map(function (item,index){
+    if ( index != 0){
+      return item[0].toUpperCase() + item.slice(1)
+    }
+    return item
+  })
+  //console.log (result)
+  return result.join('')
 }
+
+console.log (camelize('background-color') == 'backgroundColor');
+console.log (camelize('list-style-image') == 'listStyleImage');
+console.log (camelize('-webkit-transition') == 'WebkitTransition');
