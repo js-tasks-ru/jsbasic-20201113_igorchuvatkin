0e59ef7de2f7811e7527d9b988b48da06c2e904e
Igor <igorchuvatkin@gmail.com>
2020-12-13 22:20:16 +0300

    highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,33 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let age
+    let gender
+    let aval
+    for ( let i = 1; i < table.rows.length; i++){
+        for (let j = 1; j < table.rows[i].cells.length; j++){
+            if (j == 1){
+                age = table.rows[i].cells[j].innerHTML
+                if ( age < 18 ){
+                   table.rows[i].style.textDecoration = 'line-through'
+                }
+            }
+            if (j == 2){
+                gender = table.rows[i].cells[j].innerHTML
+                if (gender == 'm'){
+                    table.rows[i].classList.add('male') 
+                }
+                else table.rows[i].classList.add('female') 
+            }
+            if (j == 3){
+                aval = table.rows[i].cells[j].dataset.available
+                if (aval =='true'){
+                    table.rows[i].classList.add('available')
+                } else if (aval === 'false'){
+                    table.rows[i].classList.add('unavailable')
+                }
+                else table.rows[i].hidden = true
+            }
+        }
+    }
 }
